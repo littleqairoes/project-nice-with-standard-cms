a44b8c9e2b0cab3c35f5d178d463ab8d891d8123
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-08-21 13:58:36 +0000

    This is it one

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var exec = require('child_process').exec;
 
 var paths = {
   scripts: ['src/**/**'],
 };
 
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['shorthand']);
+  gulp.watch(paths.scripts, ['task']);
 });
 
 
-gulp.task('shorthand', function() {
-  try {
-    shell.task([
-      'echo starting...',
-      'polymer lint'
-    ])
-  } catch(e) {
-    console.log(e);
-  }
-});
+// gulp.task('shorthand',
+//   shell.task([
+//     'echo starting...',
+//     'polymer lint'
+//   ])
+// );
 
-gulp.task('default', ['watch', 'shorthand']);
+gulp.task('task', function (cb) {
+  exec('polymer lint', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('default', ['watch', 'task']);
